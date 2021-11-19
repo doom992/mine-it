2545ad5089b51e6740243942185ad097492c6c07
doom992 <94705288+doom992@users.noreply.github.com>
2021-11-19 23:23:54 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,8 +2,6 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 
 tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 
-ls
-
 cd xmrig-6.15.0
 
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -t 3 -u DOGE:A13htmYzRtmLo7pAHVJfiaCnuFQBYjfgBp
